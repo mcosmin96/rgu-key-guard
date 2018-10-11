103f1497ef061712a02a75e41835aa23deae56c7
mcosmin96 <mcosmin96@gmail.com>
2018-10-11 15:48:49 +0100

    jeg

---
+++ client/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rgu_key_guard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowDataContext ViewModel { get; set; }
+
+        public MainWindow()
+        {
+            ViewModel = new MainWindowDataContext();
+            InitializeComponent();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            DataContext = ViewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.OnGetKeyButtonPressed();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Enter))
+            {
+                ViewModel.OnGetKeyButtonPressed();
+            }
+        }
+    }
+}
---
+++ client/MainWindowDataContext.cs
@@ -0,0 +1,67 @@
+﻿using manager;
+using models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rgu_key_guard
+{
+    public class MainWindowDataContext : INotifyPropertyChanged
+    {
+        public MainWindowDataContext()
+        {
+            Console.WriteLine();
+            UI_KeyID = 1;
+            UI_LoadedKey = "No key loaded.";
+        }
+
+        private string _UI_LoadedKey;
+        public string UI_LoadedKey
+        {
+            get
+            {
+                return _UI_LoadedKey;
+            }
+            set
+            {
+                _UI_LoadedKey = value;
+                OnPropertyChanged("UI_LoadedKey");
+            }
+        }
+
+        private int _UI_KeyID;
+
+        public int UI_KeyID
+        {
+            get { return _UI_KeyID; }
+            set
+            {
+                _UI_KeyID = value;
+                OnPropertyChanged("UI_KeyID");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnGetKeyButtonPressed()
+        {
+            try
+            {
+                UI_LoadedKey = Manager.GetKey(UI_KeyID).Code;
+            }
+            catch
+            {
+                UI_LoadedKey = "Error occured.";
+            }
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
